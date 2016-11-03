fa74b9993b17692531a2633888ea76028485bb6e
vuteam <rakzami39@gmail.com>
2016-11-03 01:07:54 +0100

    Start Build

---
+++ lib/python/Components/Converter/EventName.py
@@ -0,0 +1,207 @@
+from enigma import eEPGCache
+
+from Components.Converter.Converter import Converter
+from Components.Element import cached
+from Components.Converter.genre import getGenreStringSub
+
+
+class EventName(Converter, object):
+	NAME = 0
+	SHORT_DESCRIPTION = 1
+	EXTENDED_DESCRIPTION = 2
+	FULL_DESCRIPTION = 3
+	ID = 4
+	NAME_NOW = 5
+	NAME_NEXT = 6
+	NAME_NEXT2 = 7
+	GENRE = 8
+	RATING = 9
+	SRATING = 10
+	PDC = 11
+	PDCTIME = 12
+	PDCTIMESHORT = 13
+	ISRUNNINGSTATUS = 14
+
+	NEXT_DESCRIPTION = 21
+	THIRD_NAME = 22
+	THIRD_NAME2 = 23
+	THIRD_DESCRIPTION = 24
+
+	def __init__(self, type):
+		Converter.__init__(self, type)
+		self.epgcache = eEPGCache.getInstance()
+		if type == "Description":
+			self.type = self.SHORT_DESCRIPTION
+		elif type == "ExtendedDescription":
+			self.type = self.EXTENDED_DESCRIPTION
+		elif type == "FullDescription":
+			self.type = self.FULL_DESCRIPTION
+		elif type == "ID":
+			self.type = self.ID
+		elif type == "NameNow" or type == "NowName":
+			self.type = self.NAME_NOW
+		elif type == "NameNext" or type == "NextName":
+			self.type = self.NAME_NEXT
+		elif type == "NameNextOnly" or type == "NextNameOnly":
+			self.type = self.NAME_NEXT2
+		elif type == "Genre":
+			self.type = self.GENRE
+		elif type == "Rating":
+			self.type = self.RATING
+		elif type == "SmallRating":
+			self.type = self.SRATING
+		elif type == "Pdc":
+			self.type = self.PDC
+		elif type == "PdcTime":
+			self.type = self.PDCTIME
+		elif type == "PdcTimeShort":
+			self.type = self.PDCTIMESHORT
+		elif type == "IsRunningStatus":
+			self.type = self.ISRUNNINGSTATUS
+		elif type == "NextDescription":
+			self.type = self.NEXT_DESCRIPTION
+		elif type == "ThirdName":
+			self.type = self.THIRD_NAME
+		elif type == "ThirdNameOnly":
+			self.type = self.THIRD_NAME2
+		elif type == "ThirdDescription":
+			self.type = self.THIRD_DESCRIPTION
+		else:
+			self.type = self.NAME
+
+	@cached
+	def getBoolean(self):
+		event = self.source.event
+		if event is None:
+			return False
+		if self.type == self.PDC:
+			if event.getPdcPil():
+				return True
+		return False
+
+	boolean = property(getBoolean)
+
+	@cached
+	def getText(self):
+		event = self.source.event
+		if event is None:
+			return ""
+
+		if self.type == self.NAME:
+			return event.getEventName()
+		elif self.type == self.SRATING:
+			rating = event.getParentalData()
+			if rating is None:
+				return ""
+			else:
+				country = rating.getCountryCode()
+				age = rating.getRating()
+				if age == 0:
+					return _("All ages")
+				elif age > 15:
+					return _("bc%s") % age
+				else:
+					age += 3
+					return " %d+" % age
+		elif self.type == self.RATING:
+			rating = event.getParentalData()
+			if rating is None:
+				return ""
+			else:
+				country = rating.getCountryCode()
+				age = rating.getRating()
+				if age == 0:
+					return _("Rating undefined")
+				elif age > 15:
+					return _("Rating defined by broadcaster - %d") % age
+				else:
+					age += 3
+					return _("Minimum age %d years") % age
+		elif self.type == self.GENRE:
+			genre = event.getGenreData()
+			if genre is None:
+				return ""
+			else:
+				return getGenreStringSub(genre.getLevel1(), genre.getLevel2())
+		elif self.type == self.NAME_NOW:
+			return pgettext("now/next: 'now' event label", "Now") + ": " + event.getEventName()
+		elif self.type == self.SHORT_DESCRIPTION:
+			return event.getShortDescription()
+		elif self.type == self.EXTENDED_DESCRIPTION:
+			return event.getExtendedDescription() or event.getShortDescription()
+		elif self.type == self.FULL_DESCRIPTION:
+			description = event.getShortDescription()
+			extended = event.getExtendedDescription()
+			if description and extended:
+				description += '\n'
+			return description + extended
+		elif self.type == self.ID:
+			return str(event.getEventId())
+		elif self.type == self.PDC:
+			if event.getPdcPil():
+				return _("PDC")
+			return ""
+		elif self.type in (self.PDCTIME, self.PDCTIMESHORT):
+			pil = event.getPdcPil()
+			if pil:
+				if self.type == self.PDCTIMESHORT:
+					return _("%02d:%02d") % ((pil & 0x7C0) >> 6, (pil & 0x3F))
+				return _("%d.%02d. %02d:%02d") % ((pil & 0xF8000) >> 15, (pil & 0x7800) >> 11, (pil & 0x7C0) >> 6, (pil & 0x3F))
+			return ""
+		elif self.type == self.ISRUNNINGSTATUS:
+			if event.getPdcPil():
+				running_status = event.getRunningStatus()
+				if running_status == 1:
+					return "not running"
+				if running_status == 2:
+					return "starts in a few seconds"
+				if running_status == 3:
+					return "pausing"
+				if running_status == 4:
+					return "running"
+				if running_status == 5:
+					return "service off-air"
+				if running_status in (6,7):
+					return "reserved for future use"
+				return "undefined"
+			return ""
+
+		elif int(self.type) in (6,7) or int(self.type) >= 21:
+			try:
+				reference = self.source.service
+				info = reference and self.source.info
+				if info is None:
+					return
+				test = [ 'ITSECX', (reference.toString(), 1, -1, 1440) ] # search next 24 hours
+				self.list = [] if self.epgcache is None else self.epgcache.lookupEvent(test)
+				if self.list:
+						if self.type == self.NAME_NEXT and self.list[1][1]:
+							return pgettext("now/next: 'next' event label", "Next") + ": " + self.list[1][1]
+						elif self.type == self.NAME_NEXT2 and self.list[1][1]:
+							return self.list[1][1]
+						elif self.type == self.NEXT_DESCRIPTION and (self.list[1][2] or self.list[1][3]):
+							description = self.list[1][2]
+							extended = self.list[1][3]
+							if (description and extended) and (description[0:20] != extended[0:20]):
+								description += '\n'
+							return description + extended
+						if self.type == self.THIRD_NAME and self.list[2][1]:
+							return pgettext("third event: 'third' event label", "Later") + ": " + self.list[2][1]
+						elif self.type == self.THIRD_NAME2 and self.list[2][1]:
+							return self.list[2][1]
+						elif self.type == self.THIRD_DESCRIPTION and (self.list[2][2] or self.list[2][3]):
+							description = self.list[2][2]
+							extended = self.list[2][3]
+							if (description and extended) and (description[0:20] != extended[0:20]):
+								description += '\n'
+							return description + extended
+						else:
+							# failed to return any epg data.
+							return ""
+			except:
+				# failed to return any epg data.
+				if self.type == self.NAME_NEXT:
+					return pgettext("now/next: 'next' event label", "Next") + ": " + event.getEventName()
+				return ""
+
+	text = property(getText)
---
+++ lib/python/Screens/LanguageSelection.py
@@ -0,0 +1,126 @@
+from Screens.Screen import Screen
+from Components.ActionMap import ActionMap
+from Components.Language import language
+from Components.config import config
+from Components.Sources.List import List
+from Components.Label import Label
+from Components.Sources.StaticText import StaticText
+from Components.Pixmap import Pixmap
+from Screens.InfoBar import InfoBar
+from Screens.Rc import Rc
+from Tools.Directories import resolveFilename, SCOPE_ACTIVE_SKIN
+from Tools.LoadPixmap import LoadPixmap
+
+
+def LanguageEntryComponent(file, name, index):
+	png = LoadPixmap(resolveFilename(SCOPE_ACTIVE_SKIN, "countries/" + index + ".png"))
+	if png is None:
+		png = LoadPixmap(resolveFilename(SCOPE_ACTIVE_SKIN, "countries/" + file + ".png"))
+		if png is None:
+			png = LoadPixmap(resolveFilename(SCOPE_ACTIVE_SKIN, "countries/missing.png"))
+	res = (index, name, png)
+	return res
+
+class LanguageSelection(Screen):
+	def __init__(self, session, menu_path=""):
+		Screen.__init__(self, session)
+		screentitle = _("Language selection")
+		if config.usage.show_menupath.value == 'large':
+			menu_path += screentitle
+			title = menu_path
+			self["menu_path_compressed"] = StaticText("")
+		elif config.usage.show_menupath.value == 'small':
+			title = screentitle
+			self["menu_path_compressed"] = StaticText(menu_path + " >" if not menu_path.endswith(' / ') else menu_path[:-3] + " >" or "")
+		else:
+			title = screentitle
+			self["menu_path_compressed"] = StaticText("")
+		Screen.setTitle(self, title)
+
+		self.oldActiveLanguage = language.getActiveLanguage()
+
+		self.list = []
+		self["summarylangname"] = StaticText()
+		self["languages"] = List(self.list)
+
+		self.updateList()
+		self.onLayoutFinish.append(self.selectActiveLanguage)
+
+		self["key_red"] = Label(_("Cancel"))
+		self["key_green"] = Label(_("Save"))
+
+		self["actions"] = ActionMap(["OkCancelActions", "ColorActions"],
+		{
+			"ok": self.save,
+			"cancel": self.cancel,
+			"red": self.cancel,
+			"green": self.save,
+		}, -1)
+
+	def selectActiveLanguage(self):
+		self.setTitle(self.title)
+		pos = 0
+		for pos, x in enumerate(self.list):
+			if x[0] == self.oldActiveLanguage:
+				self["languages"].index = pos
+				break
+
+	def save(self):
+		self.commit(self.run())
+		if InfoBar.instance and self.oldActiveLanguage != config.osd.language.value:
+			self.close(True)
+		else:
+			self.close()
+
+	def cancel(self):
+		language.activateLanguage(self.oldActiveLanguage)
+		self.close()
+
+	def run(self):
+		print "[LanguageSelection] updating language..."
+		lang = self["languages"].getCurrent()[0]
+		if lang != config.osd.language.value:
+			config.osd.language.setValue(lang)
+			config.osd.language.save()
+		return lang
+
+	def commit(self, lang):
+		print "[LanguageSelection] commit language"
+		language.activateLanguage(lang)
+		config.misc.languageselected.value = 0
+		config.misc.languageselected.save()
+
+	def updateList(self):
+		languageList = language.getLanguageList()
+		if not languageList: # no language available => display only english
+			list = [ LanguageEntryComponent("en", "English (UK)", "en_GB") ]
+		else:
+			list = [ LanguageEntryComponent(file = x[1][2].lower(), name = x[1][0], index = x[0]) for x in languageList]
+		self.list = list
+		self["languages"].list = list
+
+class LanguageWizard(LanguageSelection, Rc):
+	def __init__(self, session):
+		LanguageSelection.__init__(self, session)
+		Rc.__init__(self)
+		self.onLayoutFinish.append(self.selectKeys)
+		self["wizard"] = Pixmap()
+		self["summarytext"] = StaticText()
+		self["text"] = Label()
+		self.setText()
+
+	def selectKeys(self):
+		self.clearSelectedKeys()
+		self.selectKey("UP")
+		self.selectKey("DOWN")
+
+	def setText(self):
+		self["text"].setText(_("Please use the UP/DOWN keys to select your language. Then press the OK button to select it."))
+		self["summarytext"].setText(_("Please use the UP/DOWN keys to select your language. Then press the OK button to select it."))
+
+	def createSummary(self):
+		return LanguageWizardSummary
+
+class LanguageWizardSummary(Screen):
+	def __init__(self, session, parent):
+		Screen.__init__(self, session, parent)
